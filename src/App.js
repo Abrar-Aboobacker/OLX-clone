0ad9a3e2d6833323fe388593bcaa12b4b8019d4f
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-02-13 18:17:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
 import './App.css';
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
 
 function App() {
   return (
     <div>
+      <Router>
+      <Route exact path='/'>
       <Home />
+      </Route>
+      <Route path='/signup'>
+      <Signup/>
+      </Route>
+      <Route path='/login'>
+      <Login/>
+      </Route>  
+      </Router>
     </div>
   );
 }
